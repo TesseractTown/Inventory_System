6b96b26c77ff3e3d590a28ca9f00ce66d648c4d9
TesseractTown <shadowedghosts@gmail.com>
2025-03-21 17:54:20 -0700

    Basic GUI Built

--- gui.py
+++ gui.py
@@ -1,10 +1,11 @@
-from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.image import Image
 from kivy.app import App
-from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
+from kivy.lang import Builder
 import HerbManager
 import main
 import HerbRecipies
@@ -13,15 +14,72 @@ herbManager = HerbManager.HerbManager()
 herbRecipies = HerbRecipies.HerbRecipes()
 
 class GuiLayouts(App):
-    def home_page_layout(self):
-       layout = BoxLayout(orientation='vertical')
-       b1 = Button(text='button 1')
-       b2 = Button(text='button 2')
+    def build(self):
+        self.window = GridLayout()
+        self.window.cols = 1
+        self.window.size_hint = (0.6, 0.7)
+        self.window.pos_hint = {"center_x": 0.5, "center_y":0.5}
 
-       layout.add_widget(b1)
-       layout.add_widget(b2)
+        self.topLabel = Label(
+            text="Herb Manager",
+            font_size=18
+            )
+        self.window.add_widget(self.topLabel)
 
-       return layout
+        #Buttons
+
+        self.AddHerb = Button(
+            text = "Add Herb",
+            size_hint= (0.2,0.1),
+            bold= True,
+            background_color ='#00FFCE',
+        )
+        #self.AddHerb.bind(on_press=herbManager.create_herbs())
+        self.window.add_widget(self.AddHerb)
+
+
+        self.deleteHerb = Button(
+            text = "Delete Herb",
+            size_hint= (0.2,0.1),
+            bold= True,
+            background_color ='#00FFCE',
+        )
+        #self.AddHerb.bind(on_press=herbManager.delete_herb())
+        self.window.add_widget(self.deleteHerb)
+
+
+        self.editHerbAmount = Button(
+            text = "Edit Herb",
+            size_hint= (0.2,0.1),
+            bold= True,
+            background_color ='#00FFCE',
+        )
+        #self.AddHerb.bind(on_press=herbManager.remove_herb_amount())
+        self.window.add_widget(self.editHerbAmount)
+
+        self.window.add_widget(
+            Image(source="Divider.gif"))
+
+
+        self.createHerbRecipe = Button(
+            text = "Create Herb Recipe",
+            size_hint= (0.2,0.1),
+            bold= True,
+            background_color ='#00FFCE',
+        )
+        #self.AddHerb.bind(on_press=herbManager.remove_herb_amount())
+        self.window.add_widget(self.createHerbRecipe)
+
+        self.deleteHerbRecipe = Button(
+            text = "Delete Recipe",
+            size_hint= (0.2,0.1),
+            bold= True,
+            background_color ='#00FFCE',
+        )
+        #self.AddHerb.bind(on_press=herbManager.remove_herb_amount())
+        self.window.add_widget(self.deleteHerbRecipe)
+
+        return self.window
 
     
 GuiLayouts().run()
